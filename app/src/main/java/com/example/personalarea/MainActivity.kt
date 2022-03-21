c8d64160ef2365eccbec846d73fc80001f769c69
flumpyz <natalianata2001@mail.ru>
2022-03-22 00:50:23 +0500

    tariff cards added

--- app/src/main/java/com/example/personalarea/MainActivity.kt
+++ app/src/main/java/com/example/personalarea/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.personalarea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.tariffList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = TariffRecyclerAdapter(getTariffList(), getSpeedsList(), getCostsList())
+    }
+
+    private fun getTariffList(): List<String> {
+        return this.resources.getStringArray(R.array.tariff_names).toList()
+    }
+
+    private fun getSpeedsList(): List<String> {
+        return this.resources.getStringArray(R.array.tariff_speeds).toList()
+    }
+
+    private fun getCostsList(): List<String> {
+        return this.resources.getStringArray(R.array.tariff_costs).toList()
+    }
+
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..5).forEach { i -> data.add("$i element") }
+        return data
     }
 }
---
+++ app/src/main/java/com/example/personalarea/TariffRecyclerAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.personalarea
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TariffRecyclerAdapter(private val names: List<String>, private val speeds: List<String>, private val costs: List<String>):
+    RecyclerView.Adapter<TariffRecyclerAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val largeTextView: TextView = itemView.findViewById(R.id.textViewLarge)
+        val smallTextView: TextView = itemView.findViewById(R.id.textViewSmall)
+        val costTextView: TextView = itemView.findViewById(R.id.tariffCost)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.tariff_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.largeTextView.text = names[position]
+        holder.smallTextView.text = speeds[position]
+        holder.costTextView.text = costs[position]
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+    }
+}
